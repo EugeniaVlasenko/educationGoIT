adf0212be91c8dc725997e289c6b146b791fbea4
merelys <merelys.ev@gmail.com>
2017-09-24 23:17:09 +0300

    вв

---
+++ src/edu/modul_7/Client.java
@@ -0,0 +1,29 @@
+package edu.modul_7;
+
+public class Client {
+
+    private String name;
+    private int count;
+    private String type;
+
+    public Client(String name, int count, String type) {
+
+        this.name = name;
+        this.count = count;
+        this.type = type;
+
+    }
+
+    public String getType() {
+
+        return type;
+
+    }
+
+    public int getCount() {
+
+        return count;
+
+    }
+
+}
